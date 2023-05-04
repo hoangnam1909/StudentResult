95555005568c206cc59271cdcb5d4e167869fddf
hoangnam1909 <nguyenhoangn023@gmail.com>
2023-05-05 00:39:47 +0700

    vercel

--- StudentResultManagement/managementapp/managementapp/settings.py
+++ StudentResultManagement/managementapp/managementapp/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-e#&z4x5e9p3#1lw5+kt3xr*2ujz2=q)#6m&tb14zxw5)g#e5#i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
